c92f065a44f6880bdeca944a0b1d33679312e061
Jim <jimmycross@hotmail.co.uk>
2015-12-06 21:28:05 +0000

    Update Assignment.cs

--- Assignment.cs
+++ Assignment.cs
@@ -1,1 +1,41 @@
-system
+using System;
+
+class Program
+{
+
+    static string Caesar(string value, int shift)
+    {
+	char[] buffer = value.ToCharArray();
+	for (int i = 0; i < buffer.Length; i++)
+	{
+	    // Letter.
+	    char letter = buffer[i];
+	    // Add shift to all.
+	    letter = (char)(letter + shift);
+	    // Subtract 26 on overflow.
+	    // Add 26 on underflow.
+	    if (letter > 'z')
+	    {
+		letter = (char)(letter - 26);
+	    }
+	    else if (letter < 'a')
+	    {
+		letter = (char)(letter + 26);
+	    }
+	    // Store.
+	    buffer[i] = letter;
+	}
+	return new string(buffer);
+    }
+
+    static void Main()
+    {
+	string a = "KYMJR FYMJR FYNHF QLJSN";
+	string b = Caesar(a, 1); // Ok
+
+
+	Console.WriteLine(a);
+	Console.WriteLine(b);
+
+    }
+}
